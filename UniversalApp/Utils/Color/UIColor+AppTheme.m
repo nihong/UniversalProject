13618d1aedd8da3d8532a7ef6c627a8e4cea8d98
roygao <roygao666@163.com>
2020-08-04 15:04:46 +0800

    -

--- UniversalApp/Utils/Color/UIColor+AppTheme.m
+++ UniversalApp/Utils/Color/UIColor+AppTheme.m
@@ -9,6 +9,7 @@
 #import "UIColor+AppTheme.h"
 
 #define UIColorHexRGB(rgbValue ,a)  [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:a]
+
 #define HEXCOLOR(rgbValue)          [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]  //十六进制颜色
 
 @implementation UIColor (AppTheme)
